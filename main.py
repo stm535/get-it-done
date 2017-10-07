211ce7b8ade6eb2a5cf959faaef415f5ba9c670e
Shannon McCoy <mccoy2524@gmail.com>
2017-10-07 18:29:19 -0500

    added register & login html templates

--- main.py
+++ main.py
@@ -1,18 +1,94 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:get-it-done@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, name):
+        self.name = name           
+        self.completed = False 
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            user = User.query.filter_by(email=email).first()
+            if user and user.password == password:
+                # TODO - "remember" that the user has logged in
+                return redirect('/')
+            else:
+                # TODO - explain why log in failed
+                return '<h1>ERROR!</h1>'
+        
+        return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            verify = request.form['verify']
+
+            # TODO - validate user data as in 'User-Signup'
+
+            existing_user = User.query.filter_by(email=email).first()
+            if not existing_user:
+                new_user = User(email, password)
+                db.session.add(new_user)
+                db.session.commit()
+                # TODO - "remember" the user
+                return redirect('/')
+            else:
+                # TODO - user better response
+                return '<h1>Duplicate user!</h1>'
+        return render_template('register.html')    
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    return render_template('todos.html', title="GET IT Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="GET IT Done!", tasks=tasks, 
+        completed_tasks=completed_tasks)
 
-app.run()
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
